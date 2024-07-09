20e510d4e9d9121156c2ae48e79b62abeeca59ec
Steve <steve.gitprofile@outlook.com>
2024-07-09 21:03:11 +0200

    updated data file

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -2,17 +2,27 @@ const FeedbackData = [
   {
     id: 1,
     rating: 10,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    text: "React is like a magical unicorn that sprinkles joy on my code. Every time I use hooks, I feel like I'm hacking the Matrix. 10/10 would debug again.",
   },
   {
     id: 2,
     rating: 9,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    text: "I love React! It's like the Swiss Army knife of web development. Just wish it had a 'make coffee' hook. 9/10, would recommend but bring your own caffeine.",
   },
   {
     id: 3,
     rating: 8,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    text: "React is amazing, but using it with Redux sometimes feels like training a dragon. Powerful, but oh boy, does it breathe fire on my patience. 8/10, keep your fire extinguisher handy.",
+  },
+  {
+    id: 4,
+    rating: 7,
+    text: "React is like a best friend who occasionally borrows your stuff and forgets to return it. Great to hang out with, but the missing pieces can drive you nuts. 7/10, still my buddy though.",
+  },
+  {
+    id: 5,
+    rating: 6,
+    text: "React and I have a love-hate relationship. It's like dating a supermodel who insists you learn quantum physics. 6/10, she's gorgeous but my brain hurts.",
   },
 ];
 

6c44143b557c2e8dd6fb7c8ef5cf0648b8e7f688
Steve <steve.gitprofile@outlook.com>
2024-07-11 14:58:10 +0200

    added fade effect on FeedbackItems with framer-motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,23 +1,34 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
 
 function FeedbackList({ feedbackData, handleDeleteClick }) {
   return (
     <ul className="feedback-list">
-      {feedbackData.length === 0 ? (
-        <p style={{ color: "#fff", textAlign: "center" }}>No feedback yet</p>
-      ) : (
-        feedbackData.map((feedback) => (
-          <FeedbackItem
-            id={feedback.id}
-            key={feedback.id}
-            text={feedback.text}
-            rating={feedback.rating}
-            reverse={feedback.id % 2 === 0}
-            handleDeleteClick={handleDeleteClick}
-          />
-        ))
-      )}
+      <AnimatePresence>
+        {feedbackData.length === 0 ? (
+          <p style={{ color: "#fff", textAlign: "center" }}>No feedback yet</p>
+        ) : (
+          feedbackData.map((feedback) => (
+            <motion.div
+              key={feedback.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                id={feedback.id}
+                key={feedback.id}
+                text={feedback.text}
+                rating={feedback.rating}
+                reverse={feedback.id % 2 === 0}
+                handleDeleteClick={handleDeleteClick}
+              />
+            </motion.div>
+          ))
+        )}
+      </AnimatePresence>
     </ul>
   );
 }

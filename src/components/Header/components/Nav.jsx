b39b8af106d532821db7d01404903b95362dd451
Steve <steve.gitprofile@outlook.com>
2024-07-12 15:15:34 +0200

    changed Link to NavLink

--- src/components/Header/components/Nav.jsx
+++ src/components/Header/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Tooltip } from "react-tooltip";
 
 import { FaHome } from "react-icons/fa";
@@ -7,22 +7,24 @@ import { MdInfo } from "react-icons/md";
 function Nav() {
   return (
     <nav>
-      <Link
+      <NavLink
         to="/"
         className="nav-link"
+        activeClassName="active"
         data-tooltip-id="tooltip-home"
         data-tooltip-content="Home"
       >
         <FaHome size={25} />
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         to="/about"
         className="nav-link"
+        activeClassName="active"
         data-tooltip-id="tooltip-about"
         data-tooltip-content="About"
       >
         <MdInfo size={25} />
-      </Link>
+      </NavLink>
       <Tooltip id="tooltip-home" place="bottom" type="dark" effect="solid" />
       <Tooltip id="tooltip-about" place="bottom" type="dark" effect="solid" />
     </nav>
